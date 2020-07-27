66471855bbba18423d74c31408f1e0ba913ca155
XUAXUQAQ <xuanxusb@outlook.com>
2020-07-27 11:29:37 +0800

    update version

--- src/main/java/UDisk/VersionCheck/VersionCheckUtil.java
+++ src/main/java/UDisk/VersionCheck/VersionCheckUtil.java
@@ -10,7 +10,7 @@ import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 
 public class VersionCheckUtil {
-    private static final String CURRENT_VERSION = "1.1";
+    private static final String CURRENT_VERSION = "1.2";
     private static String updateURL;
 
     private static JSONObject getVersionInfo() throws IOException {
