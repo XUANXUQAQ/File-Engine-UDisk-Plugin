6def2287ff66f3b9a25817c608fbdf7243687389
xuanxuqaq <xuanxu233@outlook.com>
2022-04-03 16:40:59 +0800

    更新版本号

--- src/main/java/UDisk/VersionCheck/VersionCheckUtil.java
+++ src/main/java/UDisk/VersionCheck/VersionCheckUtil.java
@@ -10,7 +10,7 @@ import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 
 public class VersionCheckUtil {
-    private static final String CURRENT_VERSION = "1.9";
+    private static final String CURRENT_VERSION = "2.0";
     private static String updateURL;
 
     private static JSONObject getVersionInfo() throws IOException {
